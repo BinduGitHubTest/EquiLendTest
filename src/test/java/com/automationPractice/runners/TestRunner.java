721228378e5dc044eb80f2e504b4b39665d4a991
Bindu <bindu_vajinepalli@yahoo.com>
2023-11-01 10:54:19 +0000

    Added plgin for cucumber html report and json report

--- src/test/java/com/automationPractice/runners/TestRunner.java
+++ src/test/java/com/automationPractice/runners/TestRunner.java
@@ -8,10 +8,14 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
                 "html:target/default-html-reports",
-                "rerun:target/rerun.txt"},
+                "rerun:target/rerun.txt",
+                "pretty",
+                "html:target/report/cucumber.html",
+                "json:target/report/cucumber.json"},
         features = "src/test/resources/features",
         glue = "com/automationPractice/step_definitions",
-        dryRun = false
+        dryRun = false,
+        monochrome = true
 )
 public class TestRunner {
 }
